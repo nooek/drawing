c7f6e9ee03e7835eddd1e55218f6e408cd9fe4bd
nooed <73084304+nooek@users.noreply.github.com>
2022-08-04 13:54:32 -0300

    Created matchEntity tests

--- backend/src/domain/entities/matchEntity/MatchEntity.ts
+++ backend/src/domain/entities/matchEntity/MatchEntity.ts
@@ -15,7 +15,7 @@ export default class Match {
 
   public create(props: MatchI): MatchReturnI {
     if (!props.category) {
-      throw new this.MissingParamError({
+      return new this.MissingParamError({
         statusCode: 400,
         body: {
           message: "Please provide the match category",
@@ -25,7 +25,7 @@ export default class Match {
     }
 
     if (!props.creatorId) {
-      throw new this.MissingParamError({
+      return new this.MissingParamError({
         statusCode: 400,
         body: {
           message: "Please provide the creator id",
@@ -35,7 +35,7 @@ export default class Match {
     }
 
     if (!props.maxPlayers) {
-      throw new this.MissingParamError({
+      return new this.MissingParamError({
         statusCode: 400,
         body: {
           message: "Please provide the number of players",
@@ -45,7 +45,7 @@ export default class Match {
     }
 
     if (props.maxPlayers < 2 || props.maxPlayers > 12) {
-      throw new this.InvalidParamError({
+      return new this.InvalidParamError({
         statusCode: 400,
         body: {
           message: "The number of players must be between 2 and 20 characters",
@@ -54,7 +54,7 @@ export default class Match {
     }
 
     if (!props.name) {
-      throw new this.MissingParamError({
+      return new this.MissingParamError({
         statusCode: 400,
         body: {
           message: "Please provide the match name",
@@ -64,8 +64,7 @@ export default class Match {
     }
 
     if (props.name.length < 2 || props.name.length > 20) {
-      
-      throw new this.InvalidParamError({
+      return new this.InvalidParamError({
         statusCode: 400,
         body: {
           message: "The match name must have between 2 and 20 characters",
@@ -75,7 +74,7 @@ export default class Match {
 
     if (props.password) {
       if (props.password.length > 18) {
-        throw new this.InvalidParamError({
+        return new this.InvalidParamError({
           statusCode: 400,
           body: {
             message: "The match password must have less than 18 characters",
--- backend/src/domain/entities/matchEntity/match-entity.spec.ts
+++ backend/src/domain/entities/matchEntity/match-entity.spec.ts
@@ -1,9 +1,11 @@
 import Match from "./MatchEntity";
 import { InvalidParamError, MissingParamError } from "../../../utils/errors";
 
-const makeSut = () => {
+const makeSut = (params: any) => {
   const match = new Match(() => "fakeid", InvalidParamError, MissingParamError)
-  return match
+  return {
+    create: () => match.create(params)
+  }
 }
 
 describe("Test match entity", () => {
@@ -17,14 +19,70 @@ describe("Test match entity", () => {
       creatorId: "fakecreator"
     }
 
-    const sut = makeSut()
-    const match = sut.create(validEntity)
+    const sut = makeSut(validEntity)
+    const match = sut.create()
 
     expect(match.getCreatorId()).toBe(validEntity.creatorId)
   })
 
-  it("Should throw for invalid name", () => {
-    const validEntity = {
+  it("Should throw for no name", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      category: "fakecategory",
+      password: "fakepassword",
+      maxPlayers: 10,
+      creatorId: "fakecreator"
+    }
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(MissingParamError)
+  })
+
+  it("Should throw for no category", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      name: "fakematch",
+      password: "fakepassword",
+      maxPlayers: 10,
+      creatorId: "fakecreator"
+    }
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(MissingParamError)
+  })
+
+  it("Should throw for no maximum players", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      name: "fakematch",
+      category: "fakecategory",
+      password: "fakepassword",
+      creatorId: "fakecreator"
+    }
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(MissingParamError)
+  })
+
+  it("Should throw for no creatorId", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      name: "fakematch",
+      category: "fakecategory",
+      password: "fakepassword",
+      maxPlayers: 10,
+    }
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(MissingParamError)
+  })
+
+  it("Should throw for lower than allowed name length", () => {
+    const invalidEntity = {
       id: "fakeid",
       name: "x",
       category: "fakecategory",
@@ -33,11 +91,74 @@ describe("Test match entity", () => {
       creatorId: "fakecreator"
     }
 
-    const sut = makeSut()
-    const match = sut.create(validEntity)
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
 
-    console.log(match)
-
-    expect(sut.create(validEntity)).toThrow()
+    expect(match).toBeInstanceOf(InvalidParamError)
   })
+
+  it("Should throw for higher than allowed name length", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      name: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      category: "fakecategory",
+      password: "fakepassword",
+      maxPlayers: 10,
+      creatorId: "fakecreator"
+    }
+
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(InvalidParamError)
+  })
+
+  it("Should throw for higher than allowed maxPlayers", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      name: "fakematch",
+      category: "fakecategory",
+      password: "fakepassword",
+      maxPlayers: 300,
+      creatorId: "fakecreator"
+    }
+
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(InvalidParamError)
+  })
+
+  it("Should throw for lower than allowed maxPlayers", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      name: "fakematch",
+      category: "fakecategory",
+      password: "fakepassword",
+      maxPlayers: 1,
+      creatorId: "fakecreator"
+    }
+
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(InvalidParamError)
+  })
+
+  it("Should throw for higher than allowed password length", () => {
+    const invalidEntity = {
+      id: "fakeid",
+      name: "fakematch",
+      category: "fakecategory",
+      password: "fakepasswordxxxxxxxxxxxxxxxxxxxxxxxx",
+      maxPlayers: 10,
+      creatorId: "fakecreator"
+    }
+
+    const sut = makeSut(invalidEntity)
+    const match = sut.create()
+
+    expect(match).toBeInstanceOf(InvalidParamError)
+  })
+
 })
