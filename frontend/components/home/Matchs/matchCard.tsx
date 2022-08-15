82a695274e5ae1c7f3245d525ccfa5184db909b7
nooed <73084304+nooek@users.noreply.github.com>
2022-08-15 12:40:37 -0300

    Created startMatch and cancelMatch functionse

--- frontend/components/home/Matchs/matchCard.tsx
+++ frontend/components/home/Matchs/matchCard.tsx
@@ -10,10 +10,19 @@ import {
 import { MatchI } from "../../../store/matchs/action";
 
 type Props = {
-  match: MatchI
+  match: MatchI;
 }
 
 const MatchCard = ({ match }: Props) => {
+
+  const startMatch = () => {
+    console.log(match.id)
+  }
+
+  const cancelMatch = () => {
+    console.log(match.id)
+  }
+
   return (
     <MatchCardContainer>
       <MatchCardNameContainer>
@@ -25,8 +34,8 @@ const MatchCard = ({ match }: Props) => {
         <MatchCardInfo isId={true}>Match Id: {match.id}</MatchCardInfo>
       </MatchCardInfoContainer>
       <MatchCardButtonsContainer>
-        <MatchCardButton isStart={true}>Start</MatchCardButton>
-        <MatchCardButton>Cancel</MatchCardButton>
+        <MatchCardButton isStart={true} onClick={() => startMatch()}>Start</MatchCardButton>
+        <MatchCardButton onClick={() => cancelMatch()}>Cancel</MatchCardButton>
       </MatchCardButtonsContainer>
     </MatchCardContainer>
   );
